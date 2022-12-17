f2d564d7ef226e2a341e6ce10ef4217e4718ceaf
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-12-17 13:36:29 -0500

    create chess board

--- apps/daoVdao-interface/src/App.tsx
+++ apps/daoVdao-interface/src/App.tsx
@@ -1,11 +1,75 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Center, Flex, HStack, VStack } from '@chakra-ui/react'
+
+const rowLoc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+const colLoc = ['1', '2', '3', '4', '5', '6', '7', '8']
+
+enum PieceType {
+  King = 'king',
+  Queen = 'queen',
+  Knight = 'knight',
+  Bishop = 'bishop',
+  Rook = 'rook',
+  Pawn = 'pawn',
+}
+
+type PieceRuleFunc = () => void;
+
+const rules: {[key in PieceType]: PieceRuleFunc} = {
+  [PieceType.King]: () => { },
+  [PieceType.Queen]: () => { },
+  [PieceType.Knight]: () => { },
+  [PieceType.Bishop]: () => { },
+  [PieceType.Rook]: () => { },
+  [PieceType.Pawn]: () => { },
+}
+
+enum Team {
+  White,
+  Black
+}
+
+type Piece = {
+  team: Team,
+  type: PieceType
+}
+
+type Square = {
+  location: string,
+  color: string,
+  Piece?: Piece
+  rule?: PieceRuleFunc
+}
 
 function App() {
+  const chessBoardData: Square[][] = colLoc.map((vPos, vI) => rowLoc.map((hPos, hI) => {
+    const location = hPos + vPos
+    const isBlackSquare = !(vI % 2) && !(hI % 2) || vI % 2 && hI % 2
+
+    return {
+      location: location,
+      color: isBlackSquare ? 'black.900' : 'grayscale.white'
+    }
+  })).reverse()
 
   return (
-    <Box>
-    
-    </Box>
+    <Center minH='100vh'>
+      <Box>
+        {chessBoardData.map((row, i) => {
+          return (
+            <HStack key={i} gap="0">
+              {row.map((square) => {
+                return <VStack key={square.location} bg={square.color} p={12} sx={{
+                  '&': {
+                    'WebkitMarginStart': '0px !important',
+                    'marginInlineStart': '0px'
+                  }}
+                }></VStack>
+              })}
+            </HStack>
+          )
+        })}
+      </Box>
+    </Center>
   )
 }
 
--- apps/daoVdao-interface/src/providers/index.tsx
+++ apps/daoVdao-interface/src/providers/index.tsx
@@ -2,10 +2,11 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { WagmiConfig } from 'wagmi'
 import { chains, wagmiClient } from './rainbowkit/networkConfig'
+import { theme } from '@decent-org/fractal-ui'
 
 export function AppProviders({ children }: { children: JSX.Element }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <WagmiConfig client={wagmiClient}>
         <RainbowKitProvider chains={chains} modalSize='compact'>
           {children}
